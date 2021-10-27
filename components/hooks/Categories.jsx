bbb0a217911783dd7ab4b0eb1fa700e86e242858
WEST CODE <devkingdom7@gmail.com>
2021-10-27 13:55:18 +0100

    second commit

--- components/hooks/Categories.jsx
+++ components/hooks/Categories.jsx
@@ -1,38 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
 export default function Catergories({ children }) {
- const categorie = [
-  {
-   image: "/image/fashion/s95.jpg",
-   title: "Men Fashion",
-  },
-
-  {
-   image: "/image/fashion/green-bag.jpg",
-   title: "Bags",
-  },
-
-  {
-   image: "/image/fashion/s84.jpg",
-   title: "Women Fashion",
-  },
-
-  {
-   image: "/image/watches/3.jpg",
-   title: "Wrist Watches",
-  },
-
-  {
-   image: "/image/shoes/shoe1.jpg",
-   title: "Shoes",
-  },
-
-  {
-   image: "/image/health/body-care.jpg",
-   title: "Beauty",
-  },
- ];
-
  return (
   <>
    <div
@@ -44,28 +12,113 @@ export default function Catergories({ children }) {
     </h2>
    </div>
    <section className="grid grid-cols-12">
-    {categorie.map((categorieId, index) => (
-     <Link href="/components/categories/fashion/FashionCollection">
-      <div
-       className="col-span-6 md:col-span-2 px-1 hover:shadow-xl transform hover:scale-105 transition duration-500"
-       key={index}
-      >
-       <figure>
-        <Image
-         src={categorieId.image}
-         width={220}
-         height={190}
-         className="object-cover rounded-sm"
-         alt="product"
-        />
-       </figure>
+    <Link href="/components/categories/fashion/FashionCollection">
+     <div className="col-span-6 md:col-span-2 px-1 hover:shadow-xl transform hover:scale-105 transition duration-500">
+      <figure>
+       <Image
+        src="/image/fashion/s95.jpg"
+        width={220}
+        height={190}
+        className="object-cover rounded-sm"
+        alt="product"
+       />
+      </figure>
 
-       <div className="h-12 text-center py-3 text-sm text-gray-700">
-        {categorieId.title}
-       </div>
+      <div className="text-center py-3 text-sm text-gray-700">
+       <h2> Men Fashion</h2>
       </div>
-     </Link>
-    ))}
+     </div>
+    </Link>
+
+    <Link href="/components/categories/fashion/FashionCollection">
+     <div className="col-span-6 md:col-span-2 px-1 hover:shadow-xl transform hover:scale-105 transition duration-500">
+      <figure>
+       <Image
+        src="/image/fashion/green-bag.jpg"
+        width={220}
+        height={190}
+        className="object-cover rounded-sm"
+        alt="product"
+       />
+      </figure>
+
+      <div className="h-12 text-center py-3 text-sm text-gray-700">
+       <h2> Bags</h2>
+      </div>
+     </div>
+    </Link>
+
+    <Link href="/components/categories/fashion/FashionCollection">
+     <div className="col-span-6 md:col-span-2 px-1 hover:shadow-xl transform hover:scale-105 transition duration-500">
+      <figure>
+       <Image
+        src="/image/fashion/s84.jpg"
+        width={220}
+        height={190}
+        className="object-cover rounded-sm"
+        alt="product"
+       />
+      </figure>
+
+      <div className="h-12 text-center py-3 text-sm text-gray-700">
+       <h2>Women Fashion</h2>
+      </div>
+     </div>
+    </Link>
+
+    <Link href="/components/categories/fashion/FashionCollection">
+     <div className="col-span-6 md:col-span-2 px-1 hover:shadow-xl transform hover:scale-105 transition duration-500">
+      <figure>
+       <Image
+        src="/image/watches/3.jpg"
+        width={220}
+        height={190}
+        className="object-cover rounded-sm"
+        alt="product"
+       />
+      </figure>
+
+      <div className="h-12 text-center py-3 text-sm text-gray-700">
+       <h2>Wrist Watches</h2>
+      </div>
+     </div>
+    </Link>
+
+    <Link href="/components/categories/fashion/FashionCollection">
+     <div className="col-span-6 md:col-span-2 px-1 hover:shadow-xl transform hover:scale-105 transition duration-500">
+      <figure>
+       <Image
+        src="/image/shoes/shoe1.jpg"
+        width={220}
+        height={190}
+        className="object-cover rounded-sm"
+        alt="product"
+       />
+      </figure>
+
+      <div className=" text-center py-3 text-sm text-gray-700">
+       <h2> Shoes</h2>
+      </div>
+     </div>
+    </Link>
+
+    <Link href="/components/categories/fashion/FashionCollection">
+     <div className="col-span-6 md:col-span-2 px-1 hover:shadow-xl transform hover:scale-105 transition duration-500">
+      <figure>
+       <Image
+        src="/image/health/body-care.jpg"
+        width={220}
+        height={190}
+        className="object-cover rounded-sm"
+        alt="product"
+       />
+      </figure>
+
+      <div className="h-12 text-center py-3 text-sm text-gray-700">
+       <h2>Beauty and Makeup</h2>
+      </div>
+     </div>
+    </Link>
    </section>
   </>
  );
--- pages/_document.js
+++ pages/_document.js
@@ -14,20 +14,6 @@ class MyDocument extends Document {
       href="https://fonts.googleapis.com/css2?family=Lato:wght@400&display=swap"
       rel="stylesheet"
      />
-
-     <style jsx global>{`
-      html,
-      body {
-       background: #f3f4f6;
-       padding: 0;
-       margin: 0;
-       font-family: "Lato", sans-serif;
-      }
-
-      * {
-       box-sizing: border-box;
-      }
-     `}</style>
     </Head>
 
     <body>
